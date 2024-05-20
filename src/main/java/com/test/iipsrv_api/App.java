0853ff917102589ef86feff94598c243b7076a97
gayathrikk <gayathri@htic.iitm.ac.in>
2024-05-20 12:59:44 +0530

    first commit

---
+++ src/main/java/com/test/iipsrv_api/App.java
@@ -0,0 +1,13 @@
+package com.test.iipsrv_api;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/test/iipsrv_api/apollo2_iipsrv_api.java
@@ -0,0 +1,33 @@
+package com.test.iipsrv_api;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+public class apollo2_iipsrv_api {
+	 @Test(priority=1)
+	 public void iipsrv()
+	 
+	    {
+	    	Response response1 = RestAssured
+	    		    .given()
+	    		    .auth()
+	    		    .basic("admin", "admin")
+	    		    .when()
+	    		    .get("https://apollo2.humanbrain.in/iipsrv/fcgi-bin/iipsrv.fcgi?FIF=/data/storageIIT/humanbrain/analytics/52/NISL/B_52_MTB10-SL_71-ST_NISL-SE_427_compressed.jp2&WID=1024&GAM=1.4&MINMAX=1:0,255&MINMAX=2:0,255&MINMAX=3:0,255&JTL=0,0");
+
+	        int statusCode1 = response1.getStatusCode();
+	        if (statusCode1 == 200) {
+	            System.out.println("API request to the apollo2 iipsrv passed. Status code: " + statusCode1);
+	            
+	        } else {
+	            System.out.println("API request to the apollo2 iipsrv failed. Status code: " + statusCode1);
+	        }
+	        Assert.assertEquals(statusCode1, 200, "API request to the apollo2 iipsrv failed");
+	        
+	    }	
+	
+
+}
