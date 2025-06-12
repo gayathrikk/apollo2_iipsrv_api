0c852e604f1abf87f83348013caf5fa881afe909
Gayathrigayu <36566787+gayathrikk@users.noreply.github.com>
2025-06-12 12:13:56 +0530

    Update apollo2_iipsrv_api.java

--- src/test/java/com/test/iipsrv_api/apollo2_iipsrv_api.java
+++ src/test/java/com/test/iipsrv_api/apollo2_iipsrv_api.java
@@ -16,7 +16,7 @@ public class apollo2_iipsrv_api {
 	    		    .auth()
 	    		    .basic("admin", "admin")
 	    		    .when()
-	    		    .get("https://apollo2.humanbrain.in/iipsrv/fcgi-bin/iipsrv.fcgi?FIF=/data/storageIIT/humanbrain/analytics/222/NISL/B_222_FB74-SL_114-ST_NISL-SE_340_compressed.jp2&WID=1024&GAM=1.4&MINMAX=1:0,255&MINMAX=2:0,255&MINMAX=3:0,255&JTL=6,614");
+	    		    .get("https://preprod.humanbrain.in//iipsrv/fcgi-bin/iipsrv.fcgi?FIF=/ddn/storageIIT/humanbrain/analytics/222/NISL/B_222_FB74-SL_316-ST_NISL-SE_946_compressed.jp2&WID=1024&GAM=3.2&MINMAX=1:0,141&MINMAX=2:0,153&MINMAX=3:117,255&JTL=0,0");
 
 	        int statusCode1 = response1.getStatusCode();
 	        if (statusCode1 == 200) {
